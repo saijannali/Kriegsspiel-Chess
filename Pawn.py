fcab8a720d1eb258c74e5f9a3003307ba9b7fc67
Sai Jannali <saijannali@Sais-MacBook-Pro.local>
2021-04-24 16:21:31 -0400

    pawn

---
+++ Pawn.py
@@ -0,0 +1,25 @@
+class Pawn:
+    def __init__(self,side,board,x,y):
+        self.attemptExists = False
+
+    #Input: move = [x,y] []
+    #       curr_pawn = [x,y]
+    #returns bool
+    def isValidMove(self, curr_pawn, move):
+        #check all possible moves
+        #check if within board:
+        if (curr_pawn self.board[][])
+        # checks if the pawn is on the left side of the board. 
+        if curr_pawn[1] == 0:
+            xDist = move[1]-curr_pawn[1]
+            yDist = move[0]-curr_pawn[0]
+        if curr_pawn[1] == 3:
+
+        # white left pawn  = curr_pawn[0] = 1, curr_pawn[1] = 0 
+        
+        if (self.board[3][1]):
+
+        return None
+
+    def sendMoveCoord(self,side,board,x,y):
+        return None
